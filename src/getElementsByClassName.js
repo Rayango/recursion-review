4cf391ce15ee289bbdbf53256d6d8bc2002f903a
Rayango <rayango92@gmail.com>
2017-11-27 15:27:13 -0800

    finished code up to the for loop

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+  var elementsWithClassName = [];
+  node = node || document.body;
+  if (node.classList && node.classList.contains(className)) {
+    elementsWithClassName.push(node);
+  }
+  for (var i = 0; i < node.childNodes.length; i++) {
+    var childNode = (node.childNodes)[i];
+    // console.log(childNode.classList);
+    // if (childNode.classList.contains(className)) {
+    //   elementsWithClassName.push(childNode);
+    // }
+    //     
+    if () {
+      getElementsByClassName(className, childNode);
+    }   
+  } 
+  return elementsWithClassName;
 };
+
