cf6f2ea94b59cd09bf66958e9335d21e43360532
Rayango <rayango92@gmail.com>
2017-11-27 15:42:03 -0800

    completed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -12,14 +12,7 @@ var getElementsByClassName = function(className, node) {
   }
   for (var i = 0; i < node.childNodes.length; i++) {
     var childNode = (node.childNodes)[i];
-    // console.log(childNode.classList);
-    // if (childNode.classList.contains(className)) {
-    //   elementsWithClassName.push(childNode);
-    // }
-    //     
-    if () {
-      getElementsByClassName(className, childNode);
-    }   
+    elementsWithClassName = elementsWithClassName.concat(getElementsByClassName(className, childNode));
   } 
   return elementsWithClassName;
 };
